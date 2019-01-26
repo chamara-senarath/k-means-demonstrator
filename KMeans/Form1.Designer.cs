12798d8294cc4c034b063d87c5c4936415f2d639
Chamara Senarath <senarath.chamara@gmail.com>
2019-01-26 21:32:55 +0530

    Initial Commit

---
+++ KMeans/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace KMeans
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button_Load = new System.Windows.Forms.Button();
+            this.button_Itterate = new System.Windows.Forms.Button();
+            this.button_Reset = new System.Windows.Forms.Button();
+            this.openFD = new System.Windows.Forms.OpenFileDialog();
+            this.textBox_Centroids = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_Manual = new System.Windows.Forms.Button();
+            this.button_Set = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 600);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(133, 96);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(558, 433);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button_Load
+            // 
+            this.button_Load.Location = new System.Drawing.Point(818, 67);
+            this.button_Load.Name = "button_Load";
+            this.button_Load.Size = new System.Drawing.Size(154, 23);
+            this.button_Load.TabIndex = 1;
+            this.button_Load.Text = "Load File";
+            this.button_Load.UseVisualStyleBackColor = true;
+            this.button_Load.Click += new System.EventHandler(this.button_Load_Click);
+            // 
+            // button_Itterate
+            // 
+            this.button_Itterate.Location = new System.Drawing.Point(818, 163);
+            this.button_Itterate.Name = "button_Itterate";
+            this.button_Itterate.Size = new System.Drawing.Size(154, 23);
+            this.button_Itterate.TabIndex = 2;
+            this.button_Itterate.Text = "Itterate Algorithm";
+            this.button_Itterate.UseVisualStyleBackColor = true;
+            this.button_Itterate.Click += new System.EventHandler(this.button_Itterate_Click);
+            // 
+            // button_Reset
+            // 
+            this.button_Reset.Location = new System.Drawing.Point(818, 195);
+            this.button_Reset.Name = "button_Reset";
+            this.button_Reset.Size = new System.Drawing.Size(154, 23);
+            this.button_Reset.TabIndex = 3;
+            this.button_Reset.Text = "Reset";
+            this.button_Reset.UseVisualStyleBackColor = true;
+            this.button_Reset.Click += new System.EventHandler(this.button_Reset_Click);
+            // 
+            // openFD
+            // 
+            this.openFD.FileName = "openFileDialog1";
+            // 
+            // textBox_Centroids
+            // 
+            this.textBox_Centroids.Location = new System.Drawing.Point(932, 14);
+            this.textBox_Centroids.Name = "textBox_Centroids";
+            this.textBox_Centroids.Size = new System.Drawing.Size(40, 20);
+            this.textBox_Centroids.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(818, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "# of Centroids";
+            // 
+            // button_Manual
+            // 
+            this.button_Manual.Location = new System.Drawing.Point(818, 99);
+            this.button_Manual.Name = "button_Manual";
+            this.button_Manual.Size = new System.Drawing.Size(154, 23);
+            this.button_Manual.TabIndex = 6;
+            this.button_Manual.Text = "Enter Manually";
+            this.button_Manual.UseVisualStyleBackColor = true;
+            this.button_Manual.Click += new System.EventHandler(this.button_Manual_Click);
+            // 
+            // button_Set
+            // 
+            this.button_Set.Location = new System.Drawing.Point(818, 131);
+            this.button_Set.Name = "button_Set";
+            this.button_Set.Size = new System.Drawing.Size(154, 23);
+            this.button_Set.TabIndex = 7;
+            this.button_Set.Text = "Set";
+            this.button_Set.UseVisualStyleBackColor = true;
+            this.button_Set.Click += new System.EventHandler(this.button_Set_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(980, 632);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button_Set);
+            this.Controls.Add(this.button_Manual);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Centroids);
+            this.Controls.Add(this.button_Reset);
+            this.Controls.Add(this.button_Itterate);
+            this.Controls.Add(this.button_Load);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "K-Means Demonstrator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_Load;
+        private System.Windows.Forms.Button button_Itterate;
+        private System.Windows.Forms.Button button_Reset;
+        private System.Windows.Forms.OpenFileDialog openFD;
+        private System.Windows.Forms.TextBox textBox_Centroids;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_Manual;
+        private System.Windows.Forms.Button button_Set;
+        private System.Windows.Forms.PictureBox pictureBox2;
+
+    }
+}
+
---
+++ KMeans/Form1.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KMeans
+{
+    public partial class Form1 : Form
+    {
+        List<int[]> dataPoints = new List<int[]>();
+        List<int[]> centroidList = new List<int[]>();
+        List<int> clusters = new List<int>();
+
+        int centroids;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Enabled = false;
+            button_Set.Enabled = false;
+        }
+
+        private void DrawPoints(int x,int y) {
+            Brush aBrush = (Brush)Brushes.Black;
+            Graphics g = panel1.CreateGraphics();
+            g.FillEllipse(aBrush, x, y, 10, 10);
+            
+        }
+
+        private void DrawCentroids(int x, int y)
+        {
+            Brush aBrush = (Brush)Brushes.Red;
+            Graphics g = panel1.CreateGraphics();
+            g.FillEllipse(aBrush, x, y, 10, 10);
+        }
+
+        private void DisplayInitialPoints()
+        {
+            foreach (int[] data in dataPoints)
+            {
+                int x = data[0];
+                int y = data[1];
+                DrawPoints(x, y);
+
+                if (!validate(x, y))
+                {
+                    MessageBox.Show("Points exceeds the limits of the panel!", "Panel is not enough!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+            }
+            GenarateInitialCentroids(int.Parse(textBox_Centroids.Text.Trim()));
+        }
+
+        private void Itterate()
+        {
+            List<int[]> oldCentroids = new List<int[]>();
+            foreach(int[] cent in centroidList)
+            {
+                int x = cent[0];
+                int y = cent[1];
+                int[] item = { x, y };
+                oldCentroids.Add(item);
+            }
+            ClearPanel();
+            Clustering();            
+            UpdateCluster();
+
+            foreach (int[] data in dataPoints)
+            {
+                int x = data[0];
+                int y = data[1];
+                DrawPoints(x, y);
+                
+            }
+            
+
+           foreach (int[] centroid in centroidList)
+            {
+                int x = centroid[0];
+                int y = centroid[1];
+                DrawCentroids(x, y);
+         
+            }
+            if (isFinished(oldCentroids, centroidList))
+            {
+                MessageBox.Show("No Itterations needed!", "No Itterations needed!",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return;
+            }
+            
+        }
+
+        private void Clustering()
+        {
+            clusters = new List<int>();
+            foreach (int[] datapoint in dataPoints)
+            {                
+                int x1 = datapoint[0];
+                int y1 = datapoint[1];
+                List<double> temp = new List<double>();
+
+                foreach (int[] centroid in centroidList)
+                {
+                    int x2 = centroid[0];
+                    int y2 = centroid[1];
+                    double dst = Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2);
+                    temp.Add(dst);
+                    
+                }
+                int minIndex = temp.IndexOf(temp.Min());
+                clusters.Add(minIndex);
+                
+            }
+ 
+
+        }
+
+        private void UpdateCluster()
+        {
+            
+            int count = 0;
+            foreach(int[] centroid in centroidList)
+            {
+                List<int[]> myDataset = new List<int[]>();
+                int i = 0;
+                foreach(int c in clusters)
+                {
+                    if(c == count)
+                    {
+                        myDataset.Add(dataPoints[i]);
+                    }
+                   i++;
+                }
+                if (myDataset.Count != 0)
+                {
+                    int xsum = 0, ysum = 0;
+                    foreach (int[] data in myDataset)
+                    {
+                        xsum += data[0];
+                        ysum += data[1];
+                    }
+
+                    int x = xsum / myDataset.Count;
+                    int y = ysum / myDataset.Count;
+                    centroid[0] = x;
+                    centroid[1] = y;
+                }
+                count++;
+
+            }
+            
+
+
+        }
+
+        private void GenarateInitialCentroids(int num)
+        {
+            int count = 0;
+            if (num >= dataPoints.Count)
+            {
+                MessageBox.Show("Number of centroids exceeds the number of points!","Out of panel range",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+            foreach (int[] data in dataPoints)
+            {
+                if (count == num)
+                {
+                    break;
+                }
+                int xVal = data[0];
+                int yVal = data[1];
+                int[] centorid = { xVal, yVal };
+                centroidList.Add(centorid);
+                DrawCentroids(xVal, yVal);        
+                count++;
+            }
+        }
+
+        private void Reset()
+        {
+            button_Load.Enabled = true;
+            button_Manual.Enabled = true;
+            panel1.Enabled = false;
+            panel1.Invalidate();
+            dataPoints = new List<int[]>();
+            centroidList = new List<int[]>();
+            clusters = new List<int>();
+            pictureBox2.Visible = Enabled;
+        }
+
+        private void button_Load_Click(object sender, EventArgs e)
+        {
+            Reset();
+            if(!(int.TryParse(textBox_Centroids.Text,out centroids)))
+            {
+                MessageBox.Show("No Centroids Detected!","No Centroids",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+
+            string chosenFile = "";
+            openFD.InitialDirectory = "C:";
+            openFD.FileName = "";
+            openFD.Filter = "Text Files|*.txt";
+            if (openFD.ShowDialog() == DialogResult.Cancel)
+            {
+                MessageBox.Show("Operation Cancelled", "Operation Cancelled",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                chosenFile = openFD.FileName;
+                string line;
+
+                  
+                System.IO.StreamReader file =
+                    new System.IO.StreamReader(chosenFile);
+                while ((line = file.ReadLine()) != null)
+                {
+                    int temp;
+                    try
+                    {
+                        if(!((int.TryParse(line.Split(',')[0],out temp))&& (int.TryParse(line.Split(',')[1], out temp))))
+                        {
+                            MessageBox.Show("Invalid Input!", "Invalid Input!",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+                            return;
+                        }
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Invalid Input!", "Invalid Input!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        return;
+                    }
+                    int x = int.Parse(line.Split(',')[0]);
+                    int y = int.Parse(line.Split(',')[1]);
+                    int[] dataPoint = { x, y };
+                    dataPoints.Add(dataPoint);
+                }
+
+                file.Close();
+                pictureBox2.Visible = false;
+                DisplayInitialPoints();
+                button_Manual.Enabled = false;
+
+
+
+            }
+        }
+
+        private void button_Reset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void ClearPanel()
+        {
+            Brush aBrush = (Brush)Brushes.White;
+            Graphics g = panel1.CreateGraphics();
+
+            foreach (int[] centroid in centroidList)
+            {
+                int x = centroid[0];
+                int y = centroid[1];
+                g.FillEllipse(aBrush, x, y, 10, 10);
+                
+            }
+
+            foreach (int[] data in dataPoints)
+            {
+                int x = data[0];
+                int y = data[1];
+                g.FillEllipse(aBrush, x, y, 10, 10);
+            }
+
+        }
+
+        private void button_Itterate_Click(object sender, EventArgs e)
+        {
+            if (dataPoints.Count == 0)
+            {
+                MessageBox.Show("Please insert points!", "No Points!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            Itterate();
+        }
+
+        private bool validate(int x,int y)
+        {
+            if (x > panel1.Width){
+                return false;
+            }
+            if (y > panel1.Height)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool isFinished(List<int[]> oldCentroids, List<int[]> newCentroids)
+        {
+            bool equals = false;
+            int counter = 0;
+            foreach (int[] centOld in oldCentroids)
+            {
+                equals = false;
+                int xOld = centOld[0];
+                int yOld = centOld[1];
+                
+                if(newCentroids[counter][0]==xOld && newCentroids[counter][1] == yOld)
+                {
+                    equals = true;
+                }
+                else
+                {
+                    return false;
+                }
+                counter++;
+            }
+            return equals;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+            int[] point = { x, y };
+            dataPoints.Add(point);
+            DrawPoints(x, y);
+        }
+
+        private void button_Set_Click(object sender, EventArgs e)
+        {
+            if (!(int.TryParse(textBox_Centroids.Text, out centroids)))
+            {
+                MessageBox.Show("No Centroids Detected!", "No Centroids", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            panel1.Enabled = false;
+            button_Load.Enabled = false;
+            button_Manual.Enabled = false;
+            button_Set.Enabled = false;
+            DisplayInitialPoints();
+        }
+
+        private void button_Manual_Click(object sender, EventArgs e)
+        {
+            if (!(int.TryParse(textBox_Centroids.Text, out centroids)))
+            {
+                MessageBox.Show("No Centroids Detected!", "No Centroids", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            button_Load.Enabled = false;
+            panel1.Enabled = true;
+            button_Set.Enabled = true;
+            button_Manual.Enabled = false;
+            pictureBox2.Visible = false;
+
+
+        }
+    }
+}
